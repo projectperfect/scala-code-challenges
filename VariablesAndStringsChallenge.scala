954daaeb02fc06c3e0600710a704853f8d0ceeb0
Perfect <110018708+projectperfect@users.noreply.github.com>
2024-05-21 07:13:11 -0700

    Add files via upload
    finished challenge

---
+++ VariablesAndStringsChallenge.scala
@@ -0,0 +1,10 @@
+@main
+def variablesAndStringsChallenge(): Unit = {
+  val line1 = "(\\(\\"
+  val line2 = "(-.-)"
+  val line3 = "o_(\")(\")"
+
+  println(line1)
+  println(line2)
+  println(line3)
+}
